16c843ae4870efacf46be2698a601322a79d2164
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2019-04-04 12:10:48 -0500

    routing api

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = require('express')();
+const router = express.Router();
 const cookieSession = require('cookie-session');
 app.use(cookieSession({secret:"Shh! It's a secret"}));
 const controller = require("./controller/control");
@@ -7,10 +8,12 @@ const controller = require("./controller/control");
 //const session = require("./controller/session");
 const middlewares= require('./middlewares');
 
-app.post('/upload', controller.uploadimg);
+router.post('/upload', controller.uploadimg);
 
-app.get("/restaurants",controller.findAll);
-app.get("/restaurants/:id", controller.findOne);
-app.post("/restaurants", controller.create);
+router.get("/restaurants",controller.findAll);
+router.get("/restaurants/:id", controller.findOne);
+router.post("/restaurants", controller.create);
 // app.patch('/restaurants/:id', controller.update);
-app.delete('/restaurants/:id', controller.delete);
+router.delete('/restaurants/:id', controller.delete);
+
+module.exports = router;
