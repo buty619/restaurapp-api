4827306076dc3c179c1dd049bf720e16a1b85c63
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2019-04-06 13:09:00 -0500

    Update session.js

--- controller/session.js
+++ controller/session.js
@@ -8,7 +8,7 @@ exports.create =  async function(req, res, next) {
     const user = await User.authenticate(auth, password);
     if (user) {
       req.session.userId = user._id;
-      console.log(req.session.userId);
+      console.log(session.userId);
       res.cookie("restaurappCookie",req.session.userId);
       //res.status(204).send(req.session.userId);
       //res.status(204).send({});
