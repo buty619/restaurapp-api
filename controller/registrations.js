71294878616420dfa64c983cdc7ec7e832095bbf
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2019-04-08 20:10:48 -0500

    push

--- controller/registrations.js
+++ controller/registrations.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const User = require("../model/User");
 var bcrypt = require('bcrypt-nodejs');
+const fetch = require('node-fetch');
 
 exports.create = (req,res) => { 
     const hash = bcrypt.hashSync(req.body.password);  
