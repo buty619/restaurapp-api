c1ef7a843d588ec86ae5124c98c4ceee915be64d
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2020-04-11 21:22:44 -0500

    mail

--- controller/session.js
+++ controller/session.js
@@ -2,25 +2,56 @@ const app = require('express')();
 const User = require("../model/User");
 const cookieSession = require('cookie-session');
 const jwt = require('jsonwebtoken');
+const SparkPost = require('sparkpost');
+const client = new SparkPost(process.env.MAILING);
 
-exports.create =  async function(req, res, next) {  
+
+exports.create = async function (req, res, next)
+{
   const auth = req.body.auth;
   const password = req.body.password;
-  try {
+  try
+  {
     const user = await User.authenticate(auth, password);
-    if (user) {
-      const token = await jwt.sign({user:user._id},"secret key")
-      res.json({token});
-      res.status(204).send({msg : 'ok log in'});
-    } else {
+    if (user)
+    {
+      const token = await jwt.sign({ user: user._id }, "secret key")
+      res.json({ token });
+      res.status(204).send({ msg: 'ok log in' });
+    } else
+    {
       res.status(401).send({});
     }
-  } catch (err) {
+  } catch (err)
+  {
     console.log(err);
     res.status(400).send({ error: 'Authentication failed.' });
   }
 }
 
-exports.logOut = async function(req, res){
+exports.logOut = async function (req, res)
+{
   req.session = null;
+}
+
+exports.mail = async (req, res) =>
+{
+  const params = req.params;
+  try
+  {
+    client.transmissions
+      .send({
+        content: {
+          from: 'cm-punk-619@hotmail.com',
+          subject: 'Hello, World!',
+          html:
+            "<html><body><p>My cool email.</p></body></html>"
+        },
+        recipients: [{ address: 'cm-punk-619@hotmail.com' }]
+      }).then(res.status(204).send({}))
+  }
+  catch (e)
+  {
+    res.status(400).send({ error: 'Send Mail Fail.' });
+  }
 }
--- routes.js
+++ routes.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = require('express')();
 const router = express.Router();
 const cookieSession = require('cookie-session');
-app.use(cookieSession({secret:"Shh! It's a secret"}));
+app.use(cookieSession({ secret: "Shh! It's a secret" }));
 const restManage = require("./controller/restManage");
 const registrations = require("./controller/registrations");
 const session = require("./controller/session");
 //const middlewares= require('./middlewares');
 
 // ------   registration  -------  //
-router.post("/register",registrations.create);
+router.post("/register", registrations.create);
 router.post("/redirect", registrations.redirection);
 router.get("/oauth", registrations.oauth);
 router.get("/oauth/callback", registrations.oauthcall);
@@ -17,11 +17,12 @@ router.get("/oauth/callback", registrations.oauthcall);
 
 // ------   Session  -------  //
 router.post("/logIn", session.create);
+router.post("/mail", session.mail);
 // router.get("/logOut", session.logOut);
 
 // ------   restaurant manage -------  //
 router.post('/upload', restManage.uploadImg);
-router.get("/restaurants",restManage.findAll);
+router.get("/restaurants", restManage.findAll);
 router.get("/restaurants/:id", restManage.findOne);
 router.post("/restaurants", restManage.create);
 // app.patch('/restaurants/:id', controller.update);
